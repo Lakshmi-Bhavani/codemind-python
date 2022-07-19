c01bb85aab3709e960832388ec4b1a775420f26f
Lakshmi-Bhavani <84325499+Lakshmi-Bhavani@users.noreply.github.com>
2022-07-19 11:13:43 +0530

    s=list(input()) #print(s) c=0 l=[] for i in range(0,len(s)):     x=s[i]     if x!=" ":         if x=="a" or x=="e" or x=="i" or x=="o" or x=="u" or x=="A" or x=="E" or x=="I" or x=="O" or x=="U":             c+=1     if x==" " or i==len(s)-1:         l.append(c)         c=0 print(min(l))

---
+++ minimum_vowel_count_in_a_word.py
@@ -0,0 +1,13 @@
+s=list(input())
+#print(s)
+c=0
+l=[]
+for i in range(0,len(s)):
+    x=s[i]
+    if x!=" ":
+        if x=="a" or x=="e" or x=="i" or x=="o" or x=="u" or x=="A" or x=="E" or x=="I" or x=="O" or x=="U":
+            c+=1
+    if x==" " or i==len(s)-1:
+        l.append(c)
+        c=0
+print(min(l))
